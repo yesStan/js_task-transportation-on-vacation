e7801ea2daa371a94bef7d00e29bc90300a02d68
yesstan <getiffive@gmail.com>
2026-03-08 01:56:24 +0200

    values wrapped

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,13 +5,20 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const LONG_TERM_DISCOUNT_DAYS = 7;
+  const LONG_TERM_DISCOUNT_AMOUNT = 50;
+  const SHORT_TERM_DISCOUNT_DAYS = 3;
+  const SHORT_TERM_DISCOUNT_AMOUNT = 20;
+
   const costPerDay = 40;
+
   let totalCost = days * costPerDay;
 
-  if (days >= 7) {
-    totalCost -= 50;
-  } else if (days >= 3) {
-    totalCost -= 20;
+  if (days >= LONG_TERM_DISCOUNT_DAYS) {
+    return (totalCost -= LONG_TERM_DISCOUNT_AMOUNT);
+  }
+  if (days >= SHORT_TERM_DISCOUNT_DAYS) {
+    return (totalCost -= SHORT_TERM_DISCOUNT_AMOUNT);
   }
 
   return totalCost;
